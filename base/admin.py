9439e6815ff11a5a28ae1ceeb1eb61c08e6332a6
azafree <azafree@daum.net>
2025-06-10 13:19:30 +0900

    coldb

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Post
+from .models import Product, Post, Collectingdb
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -31,6 +31,19 @@ class PostAdmin(admin.ModelAdmin):
         extra_context = { 'title': '문의하기DB' }
         return super().changelist_view(request, extra_context)
 
+class CollectingdbAdmin(admin.ModelAdmin):
+    # list_filter = ('price', 'name', ) 추후에 상태 컬럼이 필요한 모델에는 
+    # 'status' ex. 완료 진행중 둘중 하나로 어드민에서 '필터' 가능하도록 할 것
+    #
+    list_filter = ('name', 'status', 'created', )
+    list_display = ('name', 'number', 'status', 'created')
+    # 
+
+
+
+    def changelist_view(self, request, extra_context = None):
+        extra_context = { 'title': '문의하기DB' }
+        return super().changelist_view(request, extra_context)
 
 admin.site.register(Post, PostAdmin)
 admin.site.site_header = '관리자 페이지'
