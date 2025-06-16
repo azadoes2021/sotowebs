bc9844066d044c9d15e9983009b44d6dd953a797
azafree <azafree@daum.net>
2025-06-16 10:34:44 +0900

    coldb2

--- base/views.py
+++ base/views.py
@@ -213,20 +213,20 @@ def success(request):
     return render(request, 'success.html', {})    
     
 def successori(request): 
-    send_mail(
-        # '[SOTOPLUS] 문의 접수가 들어왔습니다.',
-        # '문의 접수가 들어왔습니다. 관리자페이지를 확인해주세요! https://sotoplus.co.kr/admin/',
-        # 'bluewate02@naver.com',
-        # ['bluewate02@naver.com'],
-    )
+    # send_mail(
+    #     '[SOTOPLUS] 문의 접수가 들어왔습니다.',
+    #     '문의 접수가 들어왔습니다. 관리자페이지를 확인해주세요! https://sotoplus.co.kr/admin/',
+    #     'bluewate02@naver.com',
+    #     ['bluewate02@naver.com'],
+    # )
     return redirect("successree")
 def successori2(request): 
-    send_mail(
-        # '[SOTOPLUS] 검사 신청 접수가 들어왔습니다.',
-        # '검사 신청 접수가 들어왔습니다. 관리자페이지를 확인해주세요! https://sotoplus.co.kr/admin/',
-        # 'bluewate02@naver.com',
-        # ['bluewate02@naver.com'],
-    )
+    # send_mail(
+    #     '[SOTOPLUS] 검사 신청 접수가 들어왔습니다.',
+    #     '검사 신청 접수가 들어왔습니다. 관리자페이지를 확인해주세요! https://sotoplus.co.kr/admin/',
+    #     'bluewate02@naver.com',
+    #     ['bluewate02@naver.com'],
+    # )
     return redirect("successree2")
 def successree(request): 
     return render(request, 'successree.html', {})    
